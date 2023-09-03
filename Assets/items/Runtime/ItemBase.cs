a832a7b40da8400b97387a04748cbd2ebcf5f769
Christian <97773844+esuriru@users.noreply.github.com>
2023-09-03 18:02:20 +0800

    Added base field to `ItemBase`.

--- Assets/items/Runtime/ItemBase.cs
+++ Assets/items/Runtime/ItemBase.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemBase : MonoBehaviour
+public class ItemBase : ScriptableObject 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string Name { get; protected set; }
 }
