b56cdf3101f5cee4af990ca9e4e7491b9cb82e7c
Christian <97773844+esuriru@users.noreply.github.com>
2023-09-03 20:15:22 +0800

    `Name` property initialized on start and call.

--- Assets/items/Runtime/Core/ItemBase.cs
+++ Assets/items/Runtime/Core/ItemBase.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+using Esuriru.Items.Utility.ExtensionMethods;
+
 namespace Esuriru.Items
 {
     [CreateAssetMenu(fileName = "New Item", 
@@ -9,21 +11,24 @@ namespace Esuriru.Items
         #region Public 
 
         /// <summary>
-        /// Name of the item that uses the name of the Scriptable Object
+        /// Name of the item 
         /// </summary>
         public string Name 
         { 
             get 
             {
-                if (string.IsNullOrEmpty(_name))
+                // In the event that the name is not filled in at runtime,
+                // through one of Unity's message methods, initialize
+                // the name before returning  
+                if (string.IsNullOrWhiteSpace(_name))
                 {
-
+                    Init();
                 }
                 return _name;
             }
             protected set 
             {
-
+                _name = value;
             }
         }
 
@@ -31,7 +36,31 @@ namespace Esuriru.Items
 
         #region Private 
 
+        [SerializeField]
         private string _name;
+    
+        /// <summary>
+        /// Editor only unity message to fill in `Name` property
+        /// </summary>
+        private void OnValidate()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// Editor and runtime unity message to fill in `Name` property
+        /// </summary>
+        private void OnEnable()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            // TODO: Add the option to choose whether
+            // to preserve acronyms
+            _name = name.ToSentence(true);
+        }
 
         #endregion
     }
