df80383ca40de305f70f209d98bd73732b82b22b
Christian <97773844+esuriru@users.noreply.github.com>
2023-09-03 18:08:24 +0800

    Created `dev` branch.

--- Assets/items/Runtime/ItemBase.cs
+++ Assets/items/Runtime/ItemBase.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Item", 
+    menuName = "items/Scriptable Objects/Item Base")]
 public class ItemBase : ScriptableObject 
 {
+    [field: SerializeField]
     public string Name { get; protected set; }
 }
