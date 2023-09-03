3ade6a88366bf76073acef9315cc27108fdd8bd7
Christian <97773844+esuriru@users.noreply.github.com>
2023-09-04 03:12:59 +0800

    Added `ShowIfAttribute` implementation.

--- Assets/items/Runtime/Utility/ShowIfAttribute.cs
+++ Assets/items/Runtime/Utility/ShowIfAttribute.cs
@@ -1,6 +1,40 @@
 using System;
 
-public class ShowIfAttribute : Attribute
-{
+using UnityEngine;
 
+using Esuriru.Items.Utility.ExtensionMethods;
+
+namespace Esuriru.Items.Utility
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ShowIfAttribute : PropertyAttribute
+    {
+        public enum Type
+        {
+            Hide,
+            ReadOnly
+        }
+
+        public string Name { get; private set; }
+
+        public object Value { get; private set; }
+
+        public Type DisableType { get; private set; }
+
+        /// <summary>
+        /// ShowIfAttribute constructor  
+        /// </summary>
+        /// <param name="name">Name of the field/property</param>
+        /// <param name="value">Value of the field/property</param>
+        /// <param name="property">Is attribute on property</param>
+        /// <param name="disableType">Type of disable</param>
+        public ShowIfAttribute(string name, object value,
+            bool property = false, Type disableType = Type.Hide)
+        {
+            Name = property ? name.BackingField() : name;
+
+            Value = value;
+            DisableType = disableType;
+        }
+    }
 }
