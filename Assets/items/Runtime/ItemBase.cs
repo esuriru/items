5700efac416ddbb6a0abe6592a8bb2a840906945
Christian <97773844+esuriru@users.noreply.github.com>
2023-09-03 18:43:39 +0800

    Added `Esuriru.Items` namespace.

--- Assets/items/Runtime/ItemBase.cs
+++ Assets/items/Runtime/ItemBase.cs
@@ -1,25 +1,28 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Item", 
-    menuName = "items/Scriptable Objects/Item Base")]
-public class ItemBase : ScriptableObject 
+namespace Esuriru.Items
 {
-    #region Public 
-
-    /// <summary>
-    /// Name of the item that uses the name of the Scriptable Object
-    /// </summary>
-    [field: SerializeField]
-    public string Name { get; protected set; }
-
-    #endregion
-
-    #region Protected
-
-    protected void OnEnable()
+    [CreateAssetMenu(fileName = "New Item", 
+        menuName = "items/Scriptable Objects/Item Base")]
+    public class ItemBase : ScriptableObject 
     {
+        #region Public 
 
+        /// <summary>
+        /// Name of the item that uses the name of the Scriptable Object
+        /// </summary>
+        [field: SerializeField]
+        public string Name { get; protected set; }
+
+        #endregion
+
+        #region Protected
+
+        protected void OnEnable()
+        {
+
+        }
+
+        #endregion
     }
-
-    #endregion
 }
