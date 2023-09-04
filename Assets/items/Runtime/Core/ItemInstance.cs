2e8013718bddf8c5986afa96bbc99d76410cc4e5
Christian <97773844+esuriru@users.noreply.github.com>
2023-09-05 00:49:30 +0800

    Added basic implementation in `ItemInstance`.

--- Assets/items/Runtime/Core/ItemInstance.cs
+++ Assets/items/Runtime/Core/ItemInstance.cs
@@ -1,8 +1,23 @@
 
 namespace Esuriru.Items
 {
+    /// <summary>
+    /// Instanced class that attaches a count onto an ItemBase
+    /// </summary>
     public class ItemInstance
     {
+        /// <summary>
+        /// ItemInstance constructor 
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <param name="count">Amount</param>
+        public ItemInstance(ItemBase item, int count)
+        {
+            Item = item;
+            Count = count;
+        }
 
+        public ItemBase Item { get; private set; }
+        public int Count { get; private set; }
     } 
 }
