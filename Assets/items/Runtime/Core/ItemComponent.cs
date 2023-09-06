e7d5599fc4534181abcf5a4a442471338abe10ff
Christian <97773844+esuriru@users.noreply.github.com>
2023-09-06 21:04:51 +0800

    Created `ItemComponent` class.

---
+++ Assets/items/Runtime/Core/ItemComponent.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Esuriru.Items
+{
+    // TODO - Have message calls
+    // TODO - An item should have states,
+    // so maybe like an OnEquip and opposite of that
+    public class ItemComponent : ScriptableObject
+    {
+        
+    }
+}
