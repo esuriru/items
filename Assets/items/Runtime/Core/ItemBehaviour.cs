5939ccbba1e4ede9735df967a703e841c0230bab
Christian <97773844+esuriru@users.noreply.github.com>
2023-09-06 18:50:29 +0800

    Added a note.

--- Assets/items/Runtime/Core/ItemBehaviour.cs
+++ Assets/items/Runtime/Core/ItemBehaviour.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace Esuriru.Items
 {
+    // NOTE - Maybe have a way to have custom item behaviour through System.Action(s)?
     public abstract class ItemBehaviour<T> : ScriptableObject
     {
         public virtual void OnUse(T user) {}
